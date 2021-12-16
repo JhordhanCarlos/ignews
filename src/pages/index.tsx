95a3348bbf47d696672b46e2458457048bde619e
JhordhanCarlos <36717504+JhordhanCarlos@users.noreply.github.com>
2021-12-16 10:13:42 -0300

    Começando o desenvolvimento da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,21 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>
-        Hello World
-      </h1>
+      <main>
+        <section>
+          <span>👏 Hey, Welcome</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl coding"/>
+      </main>
     </>
   )
 }
