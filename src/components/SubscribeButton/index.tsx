e7c11ce939e345c56478b74588866da61d351d84
JhordhanCarlos <36717504+JhordhanCarlos@users.noreply.github.com>
2022-01-21 09:58:21 -0300

    Impedindo a duplicada de dados enquanto efetua a inscrição

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -20,7 +20,6 @@ export function SubscribeButton ({priceId}: SubscribeButtonProps){
             const response = await api.post('/subscribe')
 
             const {sessionId} = response.data
-            
             const stripe = await getStripeJs()
             
             await stripe.redirectToCheckout({sessionId})
